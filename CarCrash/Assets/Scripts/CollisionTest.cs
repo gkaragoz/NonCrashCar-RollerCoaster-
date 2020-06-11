d2b30d3c89779ea7cd35f5e152e1dfda7f38195f
GurkanYesilyurt-lab <gurkan.yesilyurt@ogr.sakarya.edu.tr>
2020-06-11 17:52:02 +0300

    Initialize project

---
+++ CarCrash/Assets/Scripts/CollisionTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    public MeshDestroy md;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        md.DestroyMesh();
+    }
+}
