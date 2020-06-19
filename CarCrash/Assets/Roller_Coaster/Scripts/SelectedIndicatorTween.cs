1e1b6dd9e0fd49f2a32b7f1f1f8bc7a892754ef4
gkaragoz <annotto3@gmail.com>
2020-06-19 09:46:15 +0300

    Add SelectedIndicator

---
+++ CarCrash/Assets/Roller_Coaster/Scripts/SelectedIndicatorTween.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedIndicatorTween : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 _toScale;
+    [SerializeField]
+    private LeanTweenType _easeType;
+    [SerializeField]
+    private float _time;
+
+    private void Awake() {
+        LeanTween.scale(this.gameObject, _toScale, _time).setEase(_easeType).setLoopPingPong();
+    }
+
+}
