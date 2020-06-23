1ccb525c64d9ed91fe077c33a69db306968de690
gkaragoz <annotto3@gmail.com>
2020-06-23 12:24:42 +0300

    Add VFX confetti on road trigger.

---
+++ CarCrash/Assets/ConfettiOnRoadTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfettiOnRoadTrigger : MonoBehaviour {
+
+    private ParticleSystem[] _VFXs = new ParticleSystem[2];
+
+    private void Awake() {
+        _VFXs[0] = GetComponent<ParticleSystem>();
+        _VFXs[1] = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            foreach (var item in _VFXs) {
+                item.Play();
+            }
+        }
+    }
+
+}
